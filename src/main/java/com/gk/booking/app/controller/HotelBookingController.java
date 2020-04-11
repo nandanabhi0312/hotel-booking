c5dc612d2673ad104639f16295d79ef67dade527
nandanabhi0312 <63308174+nandanabhi0312@users.noreply.github.com>
2020-04-10 23:08:36 -0400

    Implemented App Info

--- src/main/java/com/gk/booking/app/controller/HotelBookingController.java
+++ src/main/java/com/gk/booking/app/controller/HotelBookingController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gk.booking.app.entity.AppInfo;
+
 @RequestMapping(value = "/api/booking")
 @RestController
 public class HotelBookingController {
@@ -14,8 +16,12 @@ public class HotelBookingController {
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/info")
-	public String getAppInfo() {
-		return "This is a hotel booking restful app implemented by Gowrav Krishnamurthy for demo purposes only";
+	public AppInfo getAppInfo() {
+		
+		AppInfo appInfo = new AppInfo();
+		appInfo.setAuthor("Gowrav Krishnamurthy");
+		appInfo.setInfo("This is a hotel booking Demo app");
+		return appInfo;
 	}
 
 }
---
+++ src/main/java/com/gk/booking/app/entity/AppInfo.java
@@ -0,0 +1,24 @@
+package com.gk.booking.app.entity;
+
+public class AppInfo {
+	
+	private String author;
+	
+	private String info;
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+}
