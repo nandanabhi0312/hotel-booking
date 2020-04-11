8b5103c716ec3bfadf3c14109680fb7c51723028
nandanabhi0312 <63308174+nandanabhi0312@users.noreply.github.com>
2020-04-10 22:34:14 -0400

    Added the RequestMapping at the header level

--- src/main/java/com/gk/booking/app/controller/HotelBookingController.java
+++ src/main/java/com/gk/booking/app/controller/HotelBookingController.java
@@ -4,10 +4,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping(value = "/api/booking")
 @RestController
 public class HotelBookingController {
 	
-	@RequestMapping(method = RequestMethod.GET, value = "/api/booking/ping")
+	@RequestMapping(method = RequestMethod.GET, value = "/ping")
 	public String getAppStatus() {
 		return "Available";
 	}
