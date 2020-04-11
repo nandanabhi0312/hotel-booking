68d2300a4458221cb8d625d53627f2cfefa9a0e6
nandanabhi0312 <63308174+nandanabhi0312@users.noreply.github.com>
2020-04-10 22:29:37 -0400

    Added ping service

--- src/main/java/com/gk/booking/app/controller/HotelBookingController.java
+++ src/main/java/com/gk/booking/app/controller/HotelBookingController.java
@@ -1,8 +1,15 @@
 package com.gk.booking.app.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HotelBookingController {
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/api/booking/ping")
+	public String getAppStatus() {
+		return "Available";
+	}
 
 }
