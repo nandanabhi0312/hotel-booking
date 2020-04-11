34f8d65f9791fca63d2d5818027edf4a0fe9b0e0
nandanabhi0312 <63308174+nandanabhi0312@users.noreply.github.com>
2020-04-10 22:20:14 -0400

    Fixed an issue with Booking entity

--- src/main/java/com/gk/booking/app/entity/Booking.java
+++ src/main/java/com/gk/booking/app/entity/Booking.java
@@ -1,8 +1,14 @@
 package com.gk.booking.app.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Booking {
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
 
 }
