b3d465b7e436fb20c8dea828789d8dea124e1598
nandanabhi0312 <63308174+nandanabhi0312@users.noreply.github.com>
2020-04-11 10:59:40 -0400

    Added the h2-console and data / create SQLs

--- src/main/java/com/gk/booking/app/entity/Booking.java
+++ src/main/java/com/gk/booking/app/entity/Booking.java
@@ -1,14 +1,27 @@
 package com.gk.booking.app.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
 
 @Entity
+@Table(name = "hotel_bookings")
 public class Booking {
 	
 	@Id
 	@GeneratedValue
+	@JsonIgnore
 	private Integer id;
+	
+	@JsonProperty(value = "firstname")
+	@Column(name = "first_name")
+	private String firstname;
 
 }
