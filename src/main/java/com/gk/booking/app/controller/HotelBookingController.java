22cc2f26d6f38b5272daf4cd5317529266eac80e
nandanabhi0312 <63308174+nandanabhi0312@users.noreply.github.com>
2020-04-20 09:41:05 -0400

    Added who am i api

--- src/main/java/com/gk/booking/app/controller/HotelBookingController.java
+++ src/main/java/com/gk/booking/app/controller/HotelBookingController.java
@@ -2,9 +2,13 @@ package com.gk.booking.app.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,45 +24,59 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 
-
 @RequestMapping(value = "/api/booking")
 @RestController
 public class HotelBookingController {
-	
+
 	@Autowired
 	private HotelBookingService service;
-	
+
 	@RequestMapping(method = RequestMethod.GET, value = "/ping")
 	public String getAppStatus() {
 		return "Available";
 	}
-	
-	@RequestMapping(method = RequestMethod.GET, value = "/info", produces = {"application/json"})
+
+	@RequestMapping(method = RequestMethod.GET, value = "/info", produces = { "application/json" })
 	public AppInfo getAppInfo() {
-		
+
 		AppInfo appInfo = new AppInfo();
 		appInfo.setAuthor("Gowrav Krishnamurthy");
 		appInfo.setInfo("This is a hotel booking Demo app");
 		return appInfo;
 	}
-	
-	@RequestMapping(method = RequestMethod.GET, value = "", produces = {"application/json"})
+
+	@RequestMapping(method = RequestMethod.GET, value = "/whoami", produces = { "application/json" })
+	public String getUserInfo(@RequestHeader("accept-language") String language,
+			@RequestHeader("User-Agent") String userAgent, HttpServletRequest request) {
+
+		JSONObject userInfoObj = new JSONObject();
+
+		String ipAddress = request.getRemoteAddr();
+
+		userInfoObj.put("ipaddress", ipAddress);
+		userInfoObj.put("language", language);
+		userInfoObj.put("software", userAgent);
+
+		return userInfoObj.toString();
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "", produces = { "application/json" })
 	public List<BookingId> getBookings() {
 		return service.getBookings();
 	}
-	
-	@RequestMapping(method = RequestMethod.GET, value = "/{id}", produces = {"application/json"})
+
+	@RequestMapping(method = RequestMethod.GET, value = "/{id}", produces = { "application/json" })
 	public Booking getBooking(@PathVariable Integer id) {
 		return service.getBooking(id);
 	}
-	
-	@RequestMapping(method = RequestMethod.POST, value = "" , produces = {"application/json"})
+
+	@RequestMapping(method = RequestMethod.POST, value = "", produces = { "application/json" })
 	public @ResponseBody CreateBooking createBooking(@RequestBody Booking pBooking) {
 		return service.createBooking(pBooking);
 	}
-	
+
 	@Operation(summary = "Update / Partial update the booking", description = "Send the Updated JSON along with JWT token in Authorization header as Bearer token")
-	@RequestMapping(method = RequestMethod.PATCH, value = "/{id}" , produces = {"application/json"})
+	@RequestMapping(method = RequestMethod.PATCH, value = "/{id}", produces = { "application/json" })
 	@Parameter(in = ParameterIn.HEADER, name = "Authorization")
 	public @ResponseBody Booking updateBooking(@PathVariable Integer id, @RequestBody Booking pBooking) {
 		return service.updateBooking(id, pBooking);
